8384275b4d0e32f7fc13e950c01402d49b242de4
mattonit <malan95@gmail.com>
2021-02-14 21:55:14 +0100

    1st release

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+name = 'python-snowflake'
+package = 'snowflake'
+description = 'Lightweight library to generate unique IDs'
+url = 'https://github.com/mattonit/python-snowflake'
+author = 'Mattonit'
+author_email = 'itc@matton.it'
+license = 'MIT'
